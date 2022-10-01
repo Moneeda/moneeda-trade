b801aab18a2becf6955f9ba2ec65f4030e2c4824
captainnemo29 <sifu.blanco@gmail.com>
2022-10-01 12:20:49 +0200

    style improving view styles

--- src/views/admin/AdminProfileView.vue
+++ src/views/admin/AdminProfileView.vue
@@ -1,51 +1,68 @@
 <script stup></script>
 
 <template>
-  <div class="admin-profile">
-    <div class="profile">
-      <h1 class="profile--title">Personal Info</h1>
-      <h2 class="profile--label">Full Name</h2>
-      <p class="profile--input">John Doe</p>
-      <h2 class="profile--label">Telegram Name</h2>
-      <p class="profile--input">September 9</p>
-      <h2 class="profile--label">Email</h2>
-      <p class="profile--input">example@example.com</p>
+  <div class="admin-profile flex justify-center">
+    <div class="profile self-center p-12">
+      <h1 class="profile--title relative">Personal Info</h1>
+      <hr class="profile--title-underlined flex self-center relative" />
+      <h2 class="profile--font-medium">Full Name</h2>
+      <hr class="profile--header-underlined flex self-center " />
+      <p class="profile--font-extralight">John Doe</p>
+      <h2 class="profile--font-medium">Telegram Name</h2>
+      <hr class="profile--header-underlined flex self-center" />
+      <p class="profile--font-extralight">September 9</p>
+      <h2 class="profile--font-medium">Email</h2>
+      <hr class="profile--header-underlined flex self-center" />
+      <p class="profile--font-extralight">example@example.com</p>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+
 .admin-profile {
-  display: grid;
   height: 30rem;
   margin: 0 auto;
 }
 
 .profile {
-  align-self: center;
-  justify-self: center;
   border: 2px;
   border-style: solid;
   border-color: rgb(204 219 233);
-  padding: 3rem;
   border-radius: 1.2rem;
   width: 30rem;
 
-
   &--title {
-    position: relative;
     left: 8rem;
     color: rgb(95, 165, 227);
     padding-bottom: 1.6rem;
+    /* font-weight: 500; */
     font-size: large;
   }
 
-  &--label {
+  &--title-underlined {
+  bottom: 1.3rem;
+  left: 6rem;
+  width: 50%;
+  border: 1px solid rgb(163 218 235);
+  border-radius: 5px;
+}
+
+&--header-underlined {
+  bottom: 1.3rem;
+  left: 6rem;
+  width: 50%;
+  border: 1px solid rgb(235 210 163);
+  border-radius: 5px;
+  margin-bottom: 0.6rem;
+}
+
+  &--font-medium {
     font-weight: 500;
     color: rgb(219 129 65);
   }
 
-  &--input {
+  &--font-extralight {
     font-weight: 200;
     padding-bottom: 0.6rem;
   }
