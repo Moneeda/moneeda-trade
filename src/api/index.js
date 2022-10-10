263e3bbfaf5aa10545ea3a34f6f6f8ca940191b7
captainnemo29 <sifu.blanco@gmail.com>
2022-10-10 17:10:48 +0200

    undo changes

--- src/api/index.js
+++ src/api/index.js
@@ -5,8 +5,6 @@ import ConditionsApi from "./conditions";
 import ActionsApi from "./actions";
 import UsersApi from "./users";
 import storage from "~/services/storage";
-import router from "../router";
-
 class ApiClient {
   constructor() {
     this.client = axios.create({
@@ -15,27 +13,9 @@ class ApiClient {
     this.internalClient = axios.create({
       baseURL: `${import.meta.env.VITE_APP_API_URL}/`,
     });
-    this.internalClient.interceptors.response.use(
-      (response) => response,
-      (error) => {
-        if (error.response.status === 401) {
-          router.push({ path: "/" });
-        }
-        return Promise.reject(error);
-      }
-    );
     this.internalLabClient = axios.create({
       baseURL: `${import.meta.env.VITE_APP_LAB_API_URL}/`,
     });
-    this.internalLabClient.interceptors.response.use(
-      (response) => response,
-      (error) => {
-        console.log("error cathed");
-        if (error.response.status === 401) {
-          console.log("UNAUTHORIZED_ERROR");
-        }
-      }
-    );
     const token = storage.get("jwt");
     this.setJwt(token);
   }
